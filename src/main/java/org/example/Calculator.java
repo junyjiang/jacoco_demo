17fd2084c1e8bd8129c26fdb8cad3bdbfd72f7ba
jiangchao9 <jiangchao9@zuk.com>
2025-06-30 16:33:11 +0800

    update:dockerfile构建命令

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
-    public int subtract(int a, int b) {
+    public int subtract(int a1, int b) {
         return a - b;
     }
 }
